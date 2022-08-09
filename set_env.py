b78cad9b9003af89c9e0ab57bb23098e1e4932a7
musaubrian <musaubrian15@gmail.com>
2022-08-09 22:58:40 +0300

    move config file file .config

--- set_env.py
+++ set_env.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """setsup user `environment`"""
 
+import os
 from pathlib import Path
 import subprocess
 
@@ -12,12 +13,16 @@ def set_env():
     if false asks for required input and creates it
     """
 
-    gh_config = Path(".gh_config")
-    if gh_config.exists() is False:
+    home_path = Path.home()
+    to_conf = os.path.join(home_path, ".config")
+    full_path = Path(to_conf, ".gh_config")
+    if full_path.exists() is False:
         gh_username = input("Github username: ")
         gh_token = input("Github token: ")
-        with open(".gh_config", "w") as file:
+        with open(full_path, "w") as file:
             file.write(f"{gh_username}\n{gh_token}")
+    else:
+        raise FileExistsError("File already exists")
 
     print("\n", "="*50, "\n")
     subprocess.run(["pip", "install", "-r", "requirements.txt"])
